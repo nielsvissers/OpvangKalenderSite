96294bd368398ef683bb000e5322272cdb1e6ccc
Niels Vissers <niels.vissers@telenet.be>
2019-06-03 00:48:14 +0200

    toevoegen eerste componenten

--- AngularSite/src/app/app.module.ts
+++ AngularSite/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { KindComponent } from './kind/kind.component';
+import { KindAddComponent } from './kind-add/kind-add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    KindComponent,
+    KindAddComponent
   ],
   imports: [
     BrowserModule
---
+++ AngularSite/src/app/kind-add/kind-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kind-add',
+  templateUrl: './kind-add.component.html',
+  styleUrls: ['./kind-add.component.css']
+})
+export class KindAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
